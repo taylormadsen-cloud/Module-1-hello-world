6cdee4b56d61e380009550d0e41b414436ac56f5
taylor.Madsen <taylor.madsen@doane.edu>
2026-09-14 11:08:30 -0500

    commit

--- tendencycalc/tendencycalc.js
+++ tendencycalc/tendencycalc.js
@@ -2,10 +2,12 @@ let lowValue;
 let highValue;
 let numbers = [];
 
+/// range button click event listener
 document.getElementById("setRangeButton").addEventListener("click", function () {
+    /// etablish high and low values from input fields
     lowValue = Number(document.getElementById("low").value);
     highValue = Number(document.getElementById("high").value);
-
+/// make sure the low value is less than the high value
     if (lowValue >= highValue) {
         document.getElementById("rangeMessage").textContent =
             "make sure the low number is the lowest";
@@ -14,6 +16,7 @@ document.getElementById("setRangeButton").addEventListener("click", function () 
             "Range set from " + lowValue + " to " + highValue + ".";
 
         numbers = [];
+        /// clear the numbers, mean, and median displays
         document.getElementById("numbers").textContent = "";
         document.getElementById("mean").textContent = "";
         document.getElementById("median").textContent = "";
@@ -21,7 +24,12 @@ document.getElementById("setRangeButton").addEventListener("click", function () 
 });
 document.getElementById("addNumberButton").addEventListener("click", function () {
     let input = Number(document.getElementById("numberInput").value);
-
+    /// check if the input is a number
+    if (isNaN(input)) {
+        document.getElementById("inputMessage").textContent =
+            "enter a valid number.";
+    } else {
+/// check if the input is within the range
     if (lowValue == null || highValue == null) {
     document.getElementById("inputMessage").textContent =
         "set a range first.";
@@ -30,13 +38,14 @@ document.getElementById("addNumberButton").addEventListener("click", function ()
             "enter a number between " + lowValue + " and " + highValue + ".";
     } else {
         numbers.push(input);
-
+/// message that the number was added successfully
         document.getElementById("inputMessage").textContent =
             input + " was added successfully.";
 
         document.getElementById("numberInput").value = "";
 
         calculateResults();
+    }
     }
 });
 
